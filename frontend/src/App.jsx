ab4d37a234e202d173040d839faf75a4b176104b
Juan Manuel <juanma2176@gmail.com>
2026-08-18 10:19:41 -0500

    Actualizar conexion al nuevo backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   });
 
   useEffect(() => {
-    fetch("https://hace-milagros-api.onrender.com/api/productos")
+    fetch("https://hace-milagros-ald8.onrender.com/api/productos")
       .then((respuesta) => respuesta.json())
       .then((datos) => setProductos(datos))
       .catch(() => setProductos([]))
@@ -96,7 +96,7 @@ function App() {
     };
 
     try {
-      const respuesta = await fetch("https://hace-milagros-api.onrender.com/api/pedidos", {
+      const respuesta = await fetch("https://hace-milagros-ald8.onrender.com/api/pedidos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
